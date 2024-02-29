aa39b422c8f7676a6c1757bdd2f7baa6809ede80
Islombek1205 <islombekobidov40@gmail.com>
2024-02-29 16:48:23 +0500

    i added app.js

---
+++ app.js
@@ -0,0 +1,6 @@
+let r = 1;
+let c = Math.pow(r, 2) + Math.pow(r, 2) - 2 * r * r * Math.cos(Math.PI/180)
+let aylanaUzunligi = 360 * c;
+console.log(c);
+console.log(aylanaUzunligi);
+console.log(Math.cos(Math.PI/180));
